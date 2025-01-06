48f6565a43dfc86c1010f07070dff9c5e5695336
nageshchillale <nageshchillale18@gmail.com>
2025-01-06 15:00:08 +0530

    Add files via upload

---
+++ home.js
@@ -0,0 +1,16 @@
+const createAcButton=document.getElementById('createAcButton');
+const signInButton=document.getElementById('signInButton');
+const signInForm=document.getElementById('signIn');
+const createAcForm=document.getElementById('signup');
+const signUpButton11=document.getElementById('signUpButton2')
+
+
+createAcButton.addEventListener('click',function(){
+    window.location.href = 'createacc.html';
+  
+})
+signUpButton11.addEventListener('click',function(){
+    window.location.href = 'createacc2.html';
+  
+})
+
---
+++ register.php
@@ -0,0 +1,9 @@
+<?php
+include 'connect.php';
+if(isset($POST['signUp'])){
+    $username=$_POST['email'];
+    $password2=$_POST['password'];
+    $username2=$_POST['email2'];
+    $password2=$_POST['password2'];
+}
+?>
